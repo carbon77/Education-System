8c0dcff562183009fc61481fa91616b8f0c319b7
Igor <42415653+carbon77@users.noreply.github.com>
2023-11-22 19:43:02 +0300

    Removed Material and MaterialRepository classes

--- src/main/kotlin/com/carbon/education/repository/MaterialRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.carbon.education.repository;
-
-import com.carbon.education.model.Material
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MaterialRepository : JpaRepository<Material, Long> {
-}
