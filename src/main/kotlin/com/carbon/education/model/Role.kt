a11d3905591608035043fa853478e9dfca777027
Igor <42415653+carbon77@users.noreply.github.com>
2023-11-22 19:43:33 +0300

    Updated Role.getAuthorities method

--- src/main/kotlin/com/carbon/education/model/Role.kt
+++ src/main/kotlin/com/carbon/education/model/Role.kt
@@ -1,11 +1,12 @@
 package com.carbon.education.model
 
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 enum class Role {
     USER, TEACHER, ADMIN;
 
     fun getAuthorities(): List<GrantedAuthority> {
-        return ArrayList()
+        return listOf(SimpleGrantedAuthority("ROLE_${name}"))
     }
 }
